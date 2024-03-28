9791aa434a3ab4eae1397987e98e5e05cc7d6b32
Naz <87796344+Summry@users.noreply.github.com>
2024-03-27 22:40:09 -0700

    Update python_build.groovy

--- vars/python_build.groovy
+++ vars/python_build.groovy
@@ -4,31 +4,23 @@ def call(service, imageName) {
         environment {
             PATH = "/var/lib/jenkins/.local/bin:$PATH"
         }
-
         stages {
+            stage('Build') {
+                steps {
+                    script {
+                        sh 'python3 -m venv venv'
+                        sh '. venv/bin/activate'
+                        sh 'pip install -r requirements.txt --break-system-packages'
+                    }
+                }
+            }
             stage('Python Lint') {
                 steps {
                     script {
-                        sh """
-                            sudo apt install -y python3-pip
-                            pip install pylint
-                            pylint --fail-under=5 --disable import-error ./${service}/*.py
-                            """
+                        sh 'pylint --fail-under=5 *.py'
                     }
                 }
             }
-
-            stage('Security') {
-                steps {
-                    script {
-                        sh """
-                            pip install bandit
-                            bandit -r ./${service}
-                            """
-                    }
-                }
-            }
-
             stage('Package') {
                 when {
                     expression { env.GIT_BRANCH == 'origin/main' }
@@ -36,9 +28,16 @@ def call(service, imageName) {
                 steps {
                     withCredentials([string(credentialsId: 'DockerHub', variable: 'TOKEN')]) {
                         sh "docker login -u 'nazzywazzy' -p '$TOKEN' docker.io"
-                        sh "docker build -t ${service}:latest --tag nazzywazzy/${service}:${imageName} ${service}/"
+                        sh "docker build -t ${service}:latest --tag nazzywazzy/${service}:${imageName} ./${service}"
                         sh "docker push nazzywazzy/${service}:${imageName}"
                     }
+                }
+            }
+        }
+        post {
+            always {
+                script {
+                    sh 'unset PATH'
                 }
             }
         }
