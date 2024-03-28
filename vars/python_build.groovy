7d23258599b19bc774420aa57bbcb3e100917280
Naz <87796344+Summry@users.noreply.github.com>
2024-03-27 22:33:35 -0700

    Update python_build.groovy

--- vars/python_build.groovy
+++ vars/python_build.groovy
@@ -10,6 +10,7 @@ def call(service, imageName) {
                 steps {
                     script {
                         sh """
+                            apt install -y python3-pip
                             pip install pylint
                             pylint --fail-under=5 --disable import-error ./${service}/*.py
                             """
